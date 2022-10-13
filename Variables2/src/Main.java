2630feb10e246070e84f9c596ba150f8c270861b
Mezico <Mezico@MEZICO>
2022-10-13 19:53:56 +0100

    first commit
    Uploading all the files

---
+++ Variables2/src/Main.java
@@ -0,0 +1,22 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Swapping Two Variables
+
+		String x ="water";
+		String y = "Kool-aid";
+		String temp;
+		temp = x;
+		x=y;
+		y=temp;
+		
+		System.out.println("x: "+x);
+		System.out.println("y: "+y);
+
+		
+		
+	}
+
+}
